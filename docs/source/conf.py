7a18ecbcc39cd65961d37494c403bf45360a5861
Joakim Wallmark <wallmark.joakim@gmail.com>
2024-02-20 22:06:09 +0100

    Updates for building on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,8 @@ exclude_patterns = []
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = 'logo.png'
+html_logo = '_static/logo.png'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
 }
-html_static_path = ['_static']

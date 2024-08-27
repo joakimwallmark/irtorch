803dd39a24e0af178ebd7eb7142a86ecf08b9a2b
Joakim Wallmark <wallmark.joakim@gmail.com>
2024-08-27 21:40:45 +0200

    Version change

--- irtorch/__init__.py
+++ irtorch/__init__.py
@@ -7,7 +7,7 @@ from . import config
 from . import utils
 from . import cross_validation
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 logging.basicConfig(level=logging.INFO,
                     format="%(levelname)s: %(message)s",

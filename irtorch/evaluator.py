Fixed spelling errors and added precision, F1 and recall

irtorch/evaluator.py
@@ -274,7 +274,7 @@ class Evaluator:
         **kwargs
     ):
         """
-        Calculate the prediction accuracy of the model for the supplied data.
+        Calculate the prediction accuracy of the model for the supplied data. The response with the highest probability is considered the prediction.
 
         Parameters
         ----------
@@ -307,6 +307,88 @@ class Evaluator:
             dim = None
         
         return accuracy.nanmean(dim=dim)
+
+    @torch.no_grad()
+    def predictions(
+        self,
+        data: torch.Tensor = None,
+        theta: torch.Tensor = None,
+        **kwargs
+    ) -> pd.DataFrame:
+        r"""
+        Compute precision, recall, and F1 score for each item using the model's predictions.
+
+        This method works by first getting the predicted response (i.e. the one with the highest probability)
+        for each item, then comparing it to the true responses. For every response category of each item, it 
+        calculates the following metrics:
+
+        .. math::
+            \text{precision} = \frac{\text{true positives}}{\text{true positives} + \text{false positives}} \\
+            \text{recall} = \frac{\text{true positives}}{\text{true positives} + \text{false negatives}} \\
+            \text{F1} = \frac{2 \times \text{precision} \times \text{recall}}{\text{precision} + \text{recall}}
+
+        where true positives are the number of correct predictions,
+        false positives are the number of times the response was predicted but was incorrect,
+        and false negatives are the number of times the response was not predicted when it should have been.
+        Each metric is then averaged across all categories for each item.
+        Weighted versions based on the proportion of responses in each category are also computed.
+        Missing responses are automatically ignored.
+
+        Parameters
+        ----------
+        data : torch.Tensor
+            The input data. If not provided, the model's training data is used.
+        theta: torch.Tensor, optional
+            The latent variable theta scores for the provided data on the original theta scale.
+            If not provided, they will be computed using :meth:`irtorch.models.BaseIRTModel.latent_scores`.
+        **kwargs : dict, optional
+            Additional keyword arguments used for theta estimation. Refer to :meth:`irtorch.models.BaseIRTModel.latent_scores` for additional details.
+
+        Returns
+        -------
+        pd.DataFrame
+            A pandas dataframe with the precision, recall and F1 score (and their weighted variants) for each item. Each row corresponds to one item.
+        """
+        data, theta, missing_mask = self._evaluate_data_theta_input(data, theta, **kwargs)
+
+        probabilities = self.model.item_probabilities(theta)
+        predictions = torch.argmax(probabilities, dim=2)
+
+        precision = torch.zeros(self.model.items)
+        recall = torch.zeros(self.model.items)
+        f1 = torch.zeros(self.model.items)
+        w_precision = torch.zeros(self.model.items)
+        w_recall = torch.zeros(self.model.items)
+        w_f1 = torch.zeros(self.model.items)
+        for item in range(self.model.items):
+            item_categories = self.model.item_categories[item]
+            for resp in range(item_categories):
+                category_pred = (predictions[~missing_mask[:, item], item] == resp)
+                category_obs = (data[~missing_mask[:, item], item] == resp)
+                weight = category_obs.sum()/category_obs.numel()
+                tp = (category_obs & category_pred).sum()
+                fp = (~category_obs & category_pred).sum()
+                fn = (category_obs & ~category_pred).sum()
+                inner_precision = 0 if tp == 0 else tp/(tp + fp)
+                inner_recall = 0 if tp == 0 else tp/(tp + fn)
+                inner_f1 = 0 if inner_precision*inner_recall == 0 else 2*inner_precision*inner_recall/(inner_precision + inner_recall)
+                
+                precision[item] += inner_precision / item_categories
+                recall[item] += inner_recall / item_categories
+                f1[item] += inner_f1 / item_categories
+                w_precision[item] += inner_precision * weight
+                w_recall[item] += inner_recall * weight
+                w_f1[item] += inner_f1 * weight
+        
+        df = pd.DataFrame({
+            "precision": precision,
+            "recall": recall,
+            "f1": f1,
+            "w_precision": w_precision,
+            "w_recall": w_recall,
+            "w_f1": w_f1
+        })
+        return df
 
     @torch.no_grad()
     def infit_outfit(

tests/test_evaluator.py
@@ -33,6 +33,7 @@ def algorithm(latent_variables):
 @pytest.fixture
 def irt_model(latent_variables, algorithm: AE):
     item_categories = [2, 3]
+    items = 2
 
     def model_forward_mock(input_tensor: torch.Tensor):
         logits = [
@@ -63,7 +64,7 @@ def irt_model(latent_variables, algorithm: AE):
     mock_model.item_probabilities = MagicMock(side_effect=item_probabilities_mock)
     mock_model.latent_scores = MagicMock(side_effect=latent_scores)
     mock_model.item_categories = item_categories
-    mock_model.item_categories = item_categories
+    mock_model.items = items
     mock_model.mc_correct = None
     mock_model.latent_variables = latent_variables
 
@@ -330,6 +331,43 @@ def test_accuracy(evaluation: Evaluator):
     assert item_likelihoods.shape == (2,)  # We have 2 items
     assert item_likelihoods.dtype == torch.float32
     assert torch.all((accuracy >= 0) & (accuracy <= 1)), "All values are not between 0 and 1"
+
+def test_precisions(evaluation: Evaluator):
+    # Create synthetic test data
+    data = torch.cat(
+        [
+            torch.randint(0, item_cat, (30, 1))
+            for item_cat in evaluation.model.item_categories
+        ],
+        dim=1,
+    ).float()
+    # Introduce missing responses for testing (e.g., missing responses in the second item)
+    data[5:7, 1] = torch.nan
+
+    # Mock probabilities
+    def item_probabilities_mock(theta):
+        # static 3D tensor with dimensions (respondents, items, item categories)
+        t1 = torch.tensor(([0.55, 0.45, 0.0], [0.2, 0.35, 0.45])).unsqueeze(0).repeat(theta.shape[0] // 2, 1, 1)
+        t2 = torch.tensor(([0.15, 0.85, 0.0], [0.6, 0.05, 0.35])).unsqueeze(0).repeat(theta.shape[0] - (theta.shape[0] // 2), 1, 1)
+        return torch.cat([t1, t2])
+    
+    evaluation.model.item_probabilities = MagicMock(side_effect=item_probabilities_mock)
+
+    df = evaluation.predictions(data=data)
+
+    assert isinstance(df, pd.DataFrame), "predictions should return a pandas DataFrame"
+
+    # The number of rows should equal the number of items (in this case 2)
+    # and the DataFrame should have 6 columns for the metrics.
+    expected_rows = len(evaluation.model.item_categories)
+    expected_columns = ["precision", "recall", "f1", "w_precision", "w_recall", "w_f1"]
+    assert df.shape == (expected_rows, len(expected_columns)), f"Expected DataFrame shape ({expected_rows}, {len(expected_columns)}), got {df.shape}"
+
+    # Check that all expected columns are present.
+    for col in expected_columns:
+        assert col in df.columns, f"Missing expected column: {col}"
+        # Ensure that all metric values are within [0, 1]
+        assert df[col].between(0, 1).all(), f"Some values in {col} are outside the range [0, 1]"
 
 
 def test_residuals(evaluation: Evaluator):
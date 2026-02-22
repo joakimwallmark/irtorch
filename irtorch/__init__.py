44df3b97c436511f22660a494f8659e0d5555a58
Joakim Wallmark <wallmark.joakim@gmail.com>
2026-02-22 19:41:37 +0100

    updated version

--- irtorch/__init__.py
+++ irtorch/__init__.py
@@ -8,7 +8,7 @@ from . import load_dataset
 from .config import *
 from .utils import *
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 logging.basicConfig(level=logging.INFO,
                     format="%(levelname)s: %(message)s",
